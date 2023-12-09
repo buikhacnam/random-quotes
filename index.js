e20971fcb1fc0fa4cdb1ad282cda647ff365a866
buikhacnam <buikhacnam11@gmail.com>
2023-12-09 15:23:36 +0700

    fix: totalquotes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 class GenerateQuote {
 	filePath = './data/data.json'
 	limit = 10
+	totalQuotes = 0
 
 	constructor() {
 		this.quotes = {}
@@ -13,6 +14,7 @@ class GenerateQuote {
 		try {
 			const data = fs.readFileSync(this.filePath, 'utf8')
 			this.quotes = JSON.parse(data)
+			this.totalQuotes = Object.keys(this.quotes).length
 		} catch (err) {
 			console.error('Error reading file:', err)
 		}
@@ -23,10 +25,9 @@ class GenerateQuote {
 			numQuotes = this.limit
 		}
 
-		const totalQuotes = Object.keys(this.quotes).length
 		const randomQuotes = []
 		for (let i = 0; i < numQuotes; i++) {
-			const randomIndex = Math.floor(Math.random() * totalQuotes) + 1
+			const randomIndex = Math.floor(Math.random() * this.totalQuotes) + 1
 			randomQuotes.push(this.quotes[randomIndex.toString()])
 		}
 		return randomQuotes
